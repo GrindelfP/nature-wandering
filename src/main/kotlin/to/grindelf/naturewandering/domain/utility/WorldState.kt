d87a6ed5fe8dd0aeeed342c75c0efd06d0f8687a
GrindelfP <grindelf.perlomutrovij@gmail.com>
2025-03-02 22:35:06 +0300

    maybe need to change structure

--- src/main/kotlin/to/grindelf/naturewandering/domain/utility/WorldState.kt
+++ src/main/kotlin/to/grindelf/naturewandering/domain/utility/WorldState.kt
@@ -1,34 +1,31 @@
 package to.grindelf.naturewandering.domain.utility
 
+import to.grindelf.naturewandering.IsometricWorldConstants.WORLD_SIZE
 import to.grindelf.naturewandering.domain.animals.Bird
 import to.grindelf.naturewandering.domain.world.Tile
 
 data class WorldState(
     val tiles: MutableList<Tile>,
-    val birds: MutableList<Bird>
+    val birds: MutableList<Bird>,
+    val worldSize: Int = WORLD_SIZE
 ) {
 
     override fun toString(): String {
 
         var resultString = ""
-
         tiles.forEach { tile ->
             resultString += "$tile;"
         }
-
         resultString += "|"
-
         birds.forEach { bird ->
             resultString += "$bird;"
         }
 
         return "$resultString||"
-
     }
 
     constructor(worldStateString: String) : this(
-        worldStateString.split("|")[0].split(";").filter { it.isNotEmpty() }.mapIndexed { i, it -> Tile(i, it) }.toMutableList(),
+        worldStateString.split("|")[0].split(";").filter { it.isNotEmpty() }.mapIndexed { i, it -> Tile(i, it, worldSize - 1) }.toMutableList(),
         worldStateString.split("|")[1].split(";").filter { it.isNotEmpty() }.map { Bird(it) }.toMutableList()
     )
-
 }
--- src/test/kotlin/to/grindelf/naturewandering/domain/utility/WorldStateTest.kt
+++ src/test/kotlin/to/grindelf/naturewandering/domain/utility/WorldStateTest.kt
@@ -21,9 +21,8 @@ class WorldStateTest {
                 Bird(10.0, 10.0, 10.0, 10.0)
             )
         )
-        val testStringWorldState = "0,0,GRASS;1,0,TREE2;|0.0,0.0,0.0,0.0;10.0,10.0,10.0,10.0;|"
+        val testStringWorldState = "G1;T2;|0.0,0.0,0.0,0.0;10.0,10.0,10.0,10.0;|"
 
-        // TODO change the save - indexes are redundant, tile types can be coded with 1 or two digits
         val testWorldState = WorldState(testStringWorldState)
 
         assertThat(testWorldState).isEqualTo(trueWorldState)
